a9a857775ac2282415c00d42b5b895ae39073539
antoinekc <kimkimkimkim0719@gmail.com>
2024-05-07 01:41:08 +0200

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,6 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all
-      respond_to do |format|
-        format.html # Render index.html.erb template
-        format.json { render json: @users } # Respond with JSON format
-      end
   end
 
   # GET /users/1 or /users/1.json
@@ -31,10 +27,8 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -44,10 +38,8 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
