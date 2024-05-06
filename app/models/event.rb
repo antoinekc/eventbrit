b957ebcf1aa824602c3ec9001d79dd0acfdacb80
antoinekc <kimkimkimkim0719@gmail.com>
2024-05-07 01:04:34 +0200

    push

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,20 +1,20 @@
 class Event < ApplicationRecord
+  belongs_to :user
   has_many :attendances
   has_many :users, through: :attendances
 
   validates :start_date, presence: true
-  validates :duration, presence: true, numericality: { only_integer: true, greater_than:0, multiple_of:5 } 
-  validates :title, presence: true, length: { minimum:5, maximum:140 }
-  validates :description, presence: true, length: { minimum:5, maximum:140 }
-  validates :price, presence:true, numericality: {only_integer: true, greater_than:1, less_than:1000}
-  validate :start_date_cannot_be_in_the_past
+  validate :start_date_in_future
+  validates :duration, presence: true, numericality: { greater_than: 0, only_integer: true, multiple_of: 5 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 140 }
+  validates :description, presence: true, length: { minimum: 20, maximum: 1000 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000 }
+  validates :location, presence: true
 
   private
 
-  def start_date_cannot_be_in_the_past
-    errors.add(:start_date, "can't be in the past") if start_date.present? && start_date < DateTime.now
+  def start_date_in_future
+    errors.add(:start_date, "must be in the future") if start_date.present? && start_date <= Date.today
   end
 
 end
-
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  validates :email,
+  presence: true,
+  uniqueness: true,
+  format: { with: /\A[^@\s]+@([^@\s]+.)+[^@\s]+\z/, message: "email adress please" } 
+  validates :last_name, 
+  presence: true
+  validates :first_name, 
+  presence: true
+
+  has_many :events
   has_many :attendances
   has_many :events, through: :attendances
 end
